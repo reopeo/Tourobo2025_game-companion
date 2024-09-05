46c4667bf0051ce77dc41c6adc306cc0bf75434f
Rin Iwai <rin@eyrin.jp>
2024-09-06 04:32:35 +0900

    大体できた

--- src/Score.tsx
+++ src/Score.tsx
@@ -13,8 +13,14 @@ import {
   IconPlus,
   IconSquareFilled,
 } from '@tabler/icons-react';
-import { useState } from 'react';
-import type { Team } from './msg';
+import { useEffect, useState } from 'react';
+import type { Match, Team } from './msg';
+import * as ROSLIB from '@tier4/roslibjs-foxglove';
+import {
+  Command,
+  type UpdateScoreRequest,
+  type UpdateScoreResponse,
+} from './srv';
 
 interface Props {
   zone: 'red' | 'blue';
@@ -37,9 +43,36 @@ export function Score({ zone }: Props) {
     score: 0,
   });
 
-  const updateTeam = (fn: (prev: Team) => Partial<Team>) => {
-    setTeam((team) => ({ ...structuredClone(team), ...fn(team) }));
-  };
+  const [updateScoreSrv, setUpdateScoreSrv] =
+    useState<ROSLIB.Service<UpdateScoreRequest, UpdateScoreResponse>>();
+
+  useEffect(() => {
+    const ros = new ROSLIB.Ros({ url: 'ws://192.168.0.118:8765' });
+
+    const matchSub = new ROSLIB.Topic<Match>({
+      ros,
+      name: '/match/status',
+      messageType: 'game_state_interfaces/msg/Match',
+    });
+    matchSub.subscribe((msg) => {
+      setTeam(zone === 'red' ? msg.red_team : msg.blue_team);
+    });
+
+    const updateScoreSrv = new ROSLIB.Service<
+      UpdateScoreRequest,
+      UpdateScoreResponse
+    >({
+      ros,
+      name: `/${zone}/update_score`,
+      serviceType: 'game_state_interfaces/srv/UpdateScore',
+    });
+    setUpdateScoreSrv(updateScoreSrv);
+
+    return () => {
+      matchSub.unsubscribe();
+      ros.close();
+    };
+  }, [zone]);
 
   return (
     <Container>
@@ -50,87 +83,134 @@ export function Score({ zone }: Props) {
           </Title>
         </Paper>
 
-        <Stack gap="xs">
-          <SegmentedControl
-            fullWidth
-            size="xl"
-            color={team.is_auto ? 'green' : 'blue'}
-            value={team.is_auto ? 'auto' : 'manual'}
-            data={[
-              { value: 'manual', label: '手動' },
-              { value: 'auto', label: '自動' },
-            ]}
-            onChange={(value) =>
-              updateTeam(() => ({ is_auto: value === 'auto' }))
-            }
-          />
-        </Stack>
+        <SegmentedControl
+          fullWidth
+          size="xl"
+          color={team.is_auto ? 'green' : zone}
+          value={team.is_auto ? 'auto' : 'manual'}
+          data={[
+            { value: 'manual', label: '手動' },
+            { value: 'auto', label: '自動' },
+          ]}
+          onChange={(value) => {
+            updateScoreSrv?.callService(
+              { command: Command.IS_AUTO, data: value === 'auto' ? 1 : 0 },
+              () => {},
+            );
+          }}
+        />
 
-        <Stack gap="xs">
-          <Group gap="xs" grow>
-            <Button
-              size="xl"
-              color="gray"
-              onClick={() =>
-                updateTeam((team) => ({ seedlings: team.seedlings - 1 }))
-              }
-            >
-              <IconMinus />
-            </Button>
-            <Title size="h1" style={{ textAlign: 'center' }}>
-              {team.seedlings}
-            </Title>
-            <Button
-              size="xl"
-              color="blue"
-              onClick={() =>
-                updateTeam((team) => ({ seedlings: team.seedlings + 1 }))
-              }
-            >
-              <IconPlus />
-            </Button>
-          </Group>
-        </Stack>
-
-        <Stack gap="xs">
-          <SegmentedControl
-            fullWidth
+        <Group gap="xs" grow>
+          <Button
             size="xl"
-            color={team.immigration ? 'green' : 'blue'}
-            value={team.immigration ? 'done' : 'not_yet'}
-            data={[
-              { value: 'not_yet', label: '未入国' },
-              { value: 'done', label: '入国済' },
-            ]}
-            onChange={(value) =>
-              updateTeam(() => ({ immigration: value === 'done' }))
-            }
-          />
-        </Stack>
+            color="gray"
+            onClick={() => {
+              updateScoreSrv?.callService(
+                { command: Command.SEEDLINGS, data: -1 },
+                () => {},
+              );
+            }}
+          >
+            <IconMinus />
+          </Button>
+          <Title size="h1" style={{ textAlign: 'center' }}>
+            {team.seedlings}
+          </Title>
+          <Button
+            size="xl"
+            color={zone}
+            onClick={() => {
+              updateScoreSrv?.callService(
+                { command: Command.SEEDLINGS, data: 1 },
+                () => {},
+              );
+            }}
+          >
+            <IconPlus />
+          </Button>
+        </Group>
+
+        <Button
+          fullWidth
+          size="xl"
+          color={team.immigration ? 'green' : zone}
+          onClick={() => {
+            updateScoreSrv?.callService(
+              {
+                command: Command.IMMIGRATION,
+                data: team.immigration ? 0 : 1,
+              },
+              () => {},
+            );
+          }}
+        >
+          {team.immigration ? '入国済み' : '入国'}
+        </Button>
 
         <Group gap="xs" grow>
           <Button
             size="xl"
             color={team.type_1_a ? 'orange' : 'gray'}
-            onClick={() => updateTeam((team) => ({ type_1_a: !team.type_1_a }))}
+            onClick={() => {
+              updateScoreSrv?.callService(
+                {
+                  command: Command.TYPE_1_A,
+                  data: team.type_1_a ? 0 : 1,
+                },
+                () => {},
+              );
+            }}
           >
             <IconBoxAlignBottomFilled />
           </Button>
           <Button
             size="xl"
             color={team.type_2 ? 'green' : 'gray'}
-            onClick={() => updateTeam((team) => ({ type_2: !team.type_2 }))}
+            onClick={() => {
+              updateScoreSrv?.callService(
+                {
+                  command: Command.TYPE_2,
+                  data: team.type_2 ? 0 : 1,
+                },
+                () => {},
+              );
+            }}
           >
             <IconSquareFilled />
           </Button>
           <Button
             size="xl"
             color={team.type_1_b ? 'orange' : 'gray'}
-            onClick={() => updateTeam((team) => ({ type_1_b: !team.type_1_b }))}
+            onClick={() => {
+              updateScoreSrv?.callService(
+                {
+                  command: Command.TYPE_1_B,
+                  data: team.type_1_b ? 0 : 1,
+                },
+                () => {},
+              );
+            }}
           >
             <IconBoxAlignBottomFilled />
           </Button>
         </Group>
+
+        <Button
+          fullWidth
+          size="xl"
+          color={team.v_goal ? 'green' : zone}
+          onClick={() => {
+            updateScoreSrv?.callService(
+              {
+                command: Command.V_GOAL,
+                data: team.v_goal ? 0 : 1,
+              },
+              () => {},
+            );
+          }}
+        >
+          {team.v_goal ? 'Vゴール済み' : 'Vゴール'}
+        </Button>
       </Stack>
     </Container>
   );
