d1a8304a8678e4b6a8827da52b202a407110c354
Rin Iwai <rin@eyrin.jp>
2024-09-06 12:01:30 +0900

    管理画面

--- src/Manage.tsx
+++ src/Manage.tsx
@@ -1,6 +1,79 @@
-import { Container, Stack, Paper, Title, Button } from '@mantine/core';
+import {
+  Container,
+  Stack,
+  Paper,
+  Title,
+  Button,
+  Code,
+  SimpleGrid,
+} from '@mantine/core';
+import * as ROSLIB from '@tier4/roslibjs-foxglove';
+import { useState, useEffect } from 'react';
+import type { Match } from './msg';
 
 export function Manage() {
+  const [match, setMatch] = useState<Match>();
+
+  const [loadNextSrv, setLoadNextSrv] = useState<ROSLIB.Service>();
+  const [startSrv, setStartSrv] = useState<ROSLIB.Service>();
+  const [redRefereeCallSrv, setRedRefereeCallSrv] = useState<ROSLIB.Service>();
+  const [blueRefereeCallSrv, setBlueRefereeCallSrv] =
+    useState<ROSLIB.Service>();
+  const [confirmSrv, setConfirmSrv] = useState<ROSLIB.Service>();
+
+  useEffect(() => {
+    const ros = new ROSLIB.Ros({ url: 'ws://192.168.0.118:8765' });
+
+    const matchSub = new ROSLIB.Topic<Match>({
+      ros,
+      name: '/match/status',
+      messageType: 'game_state_interfaces/msg/Match',
+    });
+    matchSub.subscribe((msg) => {
+      setMatch(msg);
+    });
+
+    const loadNextSrv = new ROSLIB.Service({
+      ros,
+      name: '/match/load_next',
+      serviceType: 'std_srvs/srv/Empty',
+    });
+    setLoadNextSrv(loadNextSrv);
+
+    const startSrv = new ROSLIB.Service({
+      ros,
+      name: '/match/start',
+      serviceType: 'std_srvs/srv/Empty',
+    });
+    setStartSrv(startSrv);
+
+    const redRefereeCallSrv = new ROSLIB.Service({
+      ros,
+      name: '/red/referee_call',
+      serviceType: 'std_srvs/srv/Empty',
+    });
+    setRedRefereeCallSrv(redRefereeCallSrv);
+
+    const blueRefereeCallSrv = new ROSLIB.Service({
+      ros,
+      name: '/blue/referee_call',
+      serviceType: 'std_srvs/srv/Empty',
+    });
+    setBlueRefereeCallSrv(blueRefereeCallSrv);
+
+    const confirmSrv = new ROSLIB.Service({
+      ros,
+      name: '/match/confirm',
+      serviceType: 'std_srvs/srv/Empty',
+    });
+    setConfirmSrv(confirmSrv);
+
+    return () => {
+      matchSub.unsubscribe();
+      ros.close();
+    };
+  }, []);
+
   return (
     <Container>
       <Stack gap="xl" mt="md" mb="md">
@@ -10,7 +83,46 @@ export function Manage() {
           </Title>
         </Paper>
 
-        <Button size="xl">試合結果確定</Button>
+        <SimpleGrid cols={2}>
+          <Stack gap="xl" mt="md" mb="md">
+            <Button
+              size="xl"
+              onClick={() => loadNextSrv?.callService({}, () => {})}
+            >
+              次の試合を読み込み
+            </Button>
+
+            <Button
+              size="xl"
+              onClick={() => startSrv?.callService({}, () => {})}
+            >
+              試合開始
+            </Button>
+
+            <Button
+              size="xl"
+              onClick={() => redRefereeCallSrv?.callService({}, () => {})}
+            >
+              赤ゾーン審判呼出
+            </Button>
+
+            <Button
+              size="xl"
+              onClick={() => blueRefereeCallSrv?.callService({}, () => {})}
+            >
+              青ゾーン審判呼出
+            </Button>
+
+            <Button
+              size="xl"
+              onClick={() => confirmSrv?.callService({}, () => {})}
+            >
+              試合結果確定
+            </Button>
+          </Stack>
+
+          <Code block>{JSON.stringify(match, undefined, 2)}</Code>
+        </SimpleGrid>
       </Stack>
     </Container>
   );
