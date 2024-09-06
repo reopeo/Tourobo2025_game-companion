0715097dff468ca6ed46f1e7dc309f404c307db9
Rin Iwai <rin@eyrin.jp>
2024-09-06 11:39:56 +0900

    色々追加

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,23 @@
-import { Button, Container, Stack } from '@mantine/core';
+import { Button, Container, Paper, Stack, Title } from '@mantine/core';
 import { Link } from 'react-router-dom';
 
 export function Home() {
   return (
     <Container>
-      <Stack>
-        <Button color="red" component={Link} to="red">
+      <Stack gap="xl" mt="md" mb="md">
+        <Paper p="md" bg="gray">
+          <Title size="h2" c="white">
+            ホーム
+          </Title>
+        </Paper>
+
+        <Button color="red" component={Link} to="red" size="xl">
           赤ゾーン
         </Button>
-        <Button color="blue" component={Link} to="blue">
+        <Button color="blue" component={Link} to="blue" size="xl">
           青ゾーン
         </Button>
-        <Button color="gray" component={Link} to="manage">
+        <Button color="gray" component={Link} to="manage" size="xl">
           試合進行管理
         </Button>
       </Stack>
--- src/Manage.tsx
+++ src/Manage.tsx
@@ -1,3 +1,17 @@
+import { Container, Stack, Paper, Title, Button } from '@mantine/core';
+
 export function Manage() {
-  return <></>;
+  return (
+    <Container>
+      <Stack gap="xl" mt="md" mb="md">
+        <Paper p="md" bg="gray">
+          <Title size="h2" c="white">
+            試合進行管理
+          </Title>
+        </Paper>
+
+        <Button size="xl">試合結果確定</Button>
+      </Stack>
+    </Container>
+  );
 }
